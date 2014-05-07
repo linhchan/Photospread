0b95d26c317ed42661f99a16b250b1f7558b22d8
linhchan <linhchan1205@gmail.com>
2014-05-06 17:59:41 -0700

    Update on test.py

--- mysite/photo/models.py
+++ mysite/photo/models.py
@@ -16,6 +16,26 @@ class Tag(models.Model):
         return self.tag
 
 class Image(models.Model):
+    
+    """ Resizing the image for display is significant performance issue. 
+    We are nt using the cache, so for the sake of simplicity we 
+    will store copy of thumbnailsize, when an image is added. 
+
+    Django ImageField and saving them was surprisingly tricky
+    Django ImageFields are really just strings - paths relative to one's
+    projects MEDIA_ROOT, to be specific. The ImageFields save function
+    actually builds this path from a the filename, and b the path 
+    defined in the upload_to= keyword argument.
+
+    In self.image.save the last parameter, saveFalse, is very important. 
+    If save=True which it defaults to, then self.image.save will try to 
+    call the models save method  which will call itself infinitely and 
+    save copies of the image to disk until Disk Full
+    To fix this problem, just set save=False and then make sure to call 
+    the next line at some point afterwards so the model is saved to the 
+    database.
+    """
+
     title = models.CharField(max_length=60, blank=True, null=True)
     image = models.ImageField(upload_to="images/")  
     thumbnail = models.ImageField(upload_to="images/", blank=True, null=True)
@@ -31,14 +51,34 @@ class Image(models.Model):
         """Save image dimensions."""
         super(Image, self).save(*args, **kwargs)
         im = PImage.open(pjoin(settings.MEDIA_ROOT, self.image.name))
+        if hasattr(im, '_getexif'):
+            orientation = 0x0112
+            exif = im._getexif()
+            if exif is not None:
+                orientation = exif[orientation]
+                rotations = { 
+                    3: PImage.ROTATE_180,
+                    6: PImage.ROTATE_270,
+                    8: PImage.ROTATE_90
+                }   
+                if orientation in rotations:
+                    im = im.transpose(rotations[orientation])
+                    im.save(self.image.path, overwrite=True)
+        
+        im = PImage.open(pjoin(settings.MEDIA_ROOT, self.image.name))
         self.width, self.height = im.size
-
+        
         # large thumbnail to show it in front page
         fn, ext = os.path.splitext(self.image.name)
         im.thumbnail((128,128), PImage.ANTIALIAS)
         thumb_fn = fn + "-thumb2" + ext
         tf2 = NamedTemporaryFile()
-        im.save(tf2.name, "JPEG")
+        im.save(tf2.name, "JPEG", overwrite=True)
+        filename = './media/' + thumb_fn
+        try:
+            os.remove(filename)
+        except OSError:
+            pass
         self.thumbnail2.save(thumb_fn, File(open(tf2.name)), save=False)
         tf2.close()
 
@@ -46,10 +86,15 @@ class Image(models.Model):
         im.thumbnail((40,40), PImage.ANTIALIAS)
         thumb_fn = fn + "-thumb" + ext
         tf = NamedTemporaryFile()
-        im.save(tf.name, "JPEG")
+        im.save(tf.name, "JPEG",  overwrite=True)
+        filename = './media/' + thumb_fn
+        try:
+            os.remove(filename)
+        except OSError:
+            pass
         self.thumbnail.save(thumb_fn, File(open(tf.name)), save=False)
         tf.close()
-
+        
         super(Image, self).save(*args, ** kwargs)
 
 
@@ -89,4 +134,3 @@ class Album(models.Model):
         lst = ["<a href='/media/%s'>%s</a>" % (x, x.split('/')[-1]) for x in lst]
         return join(lst, ', ')
     images.allow_tags = True
-
--- mysite/photo/tests.py
+++ mysite/photo/tests.py
@@ -1,4 +1,20 @@
 from django.test import TestCase
-"""
-Tests go here
-"""
+from django.test import TransactionTestCase
+from django.contrib.auth.models import User
+from photo.models import Album, Image
+
+FIXTURE = 'photo_test_fixture.json'
+
+class AlbumTestCase(TransactionTestCase):
+    fixtures = [FIXTURE, ]
+
+    def setUp(self):
+        self.user = User.objects.get(pk=1)
+        self.albums = Album.objects.all()
+
+    def test_unicode(self):
+    	for e in ['Test', 'Work','Nature', 'Vacation']:
+    		expected = e
+    		p1 = Album(title=expected)
+    		actual = unicode(p1)
+    		self.assertEqual(expected, actual)
